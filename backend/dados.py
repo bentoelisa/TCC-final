ef7fe9fe98c7a2816a8c0966a32cf811c0dc26d3
bentoelisa <llisatheccraft@hotmail.com>
2022-06-15 21:01:50 -0300

    mudanca dados

--- backend/dados.py
+++ backend/dados.py
@@ -1,16 +1,16 @@
 import sqlite3
 
-nome_banco = "Boardgame"
+nome_banco = "boardgame"
 con = sqlite3.connect(nome_banco)
 cur = con.cursor()
 # id INTEGER, como jogar TEXT, idade INTEGER, preco REAL
 Jogos = [
     (None, 'zombicide', 'RPG', 400.00,),
-    (None, 'Dixit', Tabuleiro,250.00, 1),
-    (None, 'Banco Imobiliario', Tabuleiro, 80.00, 1)
+    (None, 'Dixit', "Tabuleiro" ,250.00,),
+    (None, 'Banco Imobiliario', "Tabuleiro", 80.00,)
 ]
 
-cur.executemany("INSERT INTO Boardgame VALUES (?, ?, ?, ?, ?, ?)", boardgame)
+cur.executemany("INSERT INTO Boardgame VALUES (?, ?, ?, ?,)", boardgame)
 
 con.commit()
 con.close()
