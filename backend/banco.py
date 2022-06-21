eaff8866abe43288fff2a465a6d91ad013c77d47
bentoelisa <llisatheccraft@hotmail.com>
2022-06-20 21:02:41 -0300

    banco

--- backend/banco.py
+++ backend/banco.py
@@ -2,12 +2,12 @@ import sqlite3 # importa o módulo sqlite3
 import os.path # importa o módulo os.path, para usar a exists()
 import os      # importa o módulo os, para usar a unlink()
 
-nome_banco = "boargame"
+nome_banco = "boardgame"
 
 if os.path.exists(nome_banco): # Exclui o arquivo caso ele exista
     os.unlink(nome_banco)
 
-conexao = sqlite3.connect(nome_banco) # Conecta no banco
+conexao = sqlite3.connect("boardgame") # Conecta no banco
 cursor = conexao.cursor() # Obtém o cursor para o banco
 
 consulta = """CREATE TABLE boardgame (
