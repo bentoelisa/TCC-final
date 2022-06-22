f16a375300abf94422ca4bb75c8aeefb540e190c
bentoelisa <llisatheccraft@hotmail.com>
2022-06-22 17:30:30 -0300

    dados

--- backend/dados.py
+++ backend/dados.py
@@ -8,7 +8,7 @@ boardgame = [
     (None,'zombicide','RPG', 12, 400.00,),
     (None,'Dixit','Tabuleiro', 10 ,250.00,),
     (None,'Banco Imobiliario', 'Tabuleiro', 10,80.00,),
-    (None,'Cashn Guns','Tabuleiro',10, 215,00)
+    (None,'Cashn Guns','Tabuleiro',10,215,00)
     ]
 cur.executemany("INSERT INTO boardgame VALUES (?,?,?,?,?)", boardgame)
 
