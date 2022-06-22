5a345f3990f4d85a2c6cfa4b4ea463e73a30954d
bentoelisa <llisatheccraft@hotmail.com>
2022-06-22 17:10:54 -0300

    banco

--- backend/banco.py
+++ backend/banco.py
@@ -2,12 +2,12 @@ import sqlite3 # importa o módulo sqlite3
 import os.path # importa o módulo os.path, para usar a exists()
 import os      # importa o módulo os, para usar a unlink()
 
-nome_banco = "boardgame"
+nome_banco = "boardgame.db"
 
 if os.path.exists(nome_banco): # Exclui o arquivo caso ele exista
     os.unlink(nome_banco)
 
-conexao = sqlite3.connect("boardgame") # Conecta no banco
+conexao = sqlite3.connect(nome_banco) # Conecta no banco
 cursor = conexao.cursor() # Obtém o cursor para o banco
 
 consulta = """CREATE TABLE boardgame (
--- backend/dados.py
+++ backend/dados.py
@@ -5,12 +5,12 @@ con = sqlite3.connect(nome_banco)
 cur = con.cursor()
 # id INTEGER, como jogar TEXT, idade INTEGER, preco REAL
 boardgame = [
-    ('zombicide', 'RPG', 12, 400.00,),
-    ('Dixit', 'Tabuleiro', 10 ,250.00,),
-    ('Banco Imobiliario', 'Tabuleiro', 10, 80.00,),
-    ('Cashn Guns','Tabuleiro',10, 215,00)
+    ('None','zombicide', 'RPG', 12, 400.00,),
+    ('None','Dixit', 'Tabuleiro', 10 ,250.00,),
+    ('None','Banco Imobiliario', 'Tabuleiro', 10, 80.00,),
+    ('None','Cashn Guns','Tabuleiro',10, 215,00)
     ]
-cur.executemany("INSERT INTO boardgame VALUES (?,?,?,?)", boardgame)
+cur.executemany("INSERT INTO boardgame VALUES (?,?,?,?,?)", boardgame)
 
 con.commit()
 con.close()
