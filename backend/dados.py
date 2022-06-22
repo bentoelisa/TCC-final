2b2ed4d3671efd36d764beaf1daffe0ab7c8846f
bentoelisa <llisatheccraft@hotmail.com>
2022-06-22 14:42:27 -0300

    altercao dados

--- backend/dados.py
+++ backend/dados.py
@@ -6,11 +6,12 @@ cur = con.cursor()
 # id INTEGER, como jogar TEXT, idade INTEGER, preco REAL
 Jogos = [
     ('zombicide', 'RPG', 12,  400.00,),
-    ('Dixit', 'Tabuleiro', 10 ,250.00,)
+    ('Dixit', 'Tabuleiro', 10 ,250.00,),
     ('Banco Imobiliario', 'Tabuleiro', 10,  80.00,)
+    ('Cashn Guns','Tabuleiro',10, 215,00)
     ]
 
-cur.executemany("INSERT INTO Boardgame VALUES (?, ?, ?, ?,)", "boardgame")
+cur.executemany("INSERT INTO Boardgame VALUES (?, ?, ?, ?,?)", "boardgame")
 
 con.commit()
 con.close()
